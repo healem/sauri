df64bdc01f09ea8761c59dc323350ef90be0c58b
Mike Healey <healem@alum.rpi.edu>
2017-11-16 16:23:21 -0500

    Fix up packaging

--- setup.py
+++ setup.py
@@ -1,14 +1,24 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='Sauri',
     author='Mike Healey',
     author_email='healem@alum.rpi.edu',
-    url='https://github.com/healem/sauron',
+    url='https://github.com/healem/sauri',
     version='0.1',
-    packages=['sauri.common',
+    packages=['sauri',
+              'sauri.common',
               'sauri.messaging',
-              'sauri.sensors',],
+              'sauri.messaging.blocking',
+              'sauri.messaging.blocking.test',
+              'sauri.sensors',
+              'sauri.sensors.test',
+              'sauri.sensors.temperature',
+              'sauri.sensors.temperature.test',],
+    install_requires=['mock',
+                      'coverage',
+                      'PyYAML',
+                      'pika',],
     license='MIT',
     long_description=open('README').read(),
 )
